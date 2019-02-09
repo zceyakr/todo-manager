23d61c2298ff216d6dd7972c50ab73c558d69cbf
Zackery Hitchcock <zackeryhitchcock@live.com>
2019-02-09 15:00:16 -0500

    My todo list is now officially working

--- manager.py
+++ manager.py
@@ -8,31 +8,31 @@ class Manager(object):
         lines = file.readlines()
 
         for line in lines:
-            section = line.split("|")
-            print(f"Task: {section[0]}")
+            section = line.split("|||")
+            print(f"\nTask: {section[0]}")
             print(f"When it was made: {section[1]}")
             if section[2] == "True\n":
-                print("Is it done?: Completed")
+                print("Is it done?: DONE")
             else:
-                print("Is it done?: Not Completed")
+                print("Is it done?: NOT DONE")
 
     def add(self, item):
         file = open("todos.txt", "a")
-        file.write(f"{item.task} | {item.time} | {item.is_task_complete}\n")
+        file.write(f"{item.task}|||{item.time}|||{item.is_task_complete}\n")
         file.close()
 
-    def complete(self,thing):
+    def complete(self, thing):
         file = open("todos.txt", "r+")
 
         lines = file.readlines()
 
         index = 0
         for line in lines:
-            if job in line:
-                section = line.split("|")
+            if thing in line:
+                section = line.split("|||")
                 if section[2] == "False\n":
-                    section[2] = 'True\n'
-                    lines[index] = f"{section[0]},{section[1]},{section[2]}"
+                    section[2] = "True\n"
+                    lines[index] = f"{section[0]}|||{section[1]}|||{section[2]}"
                     break
 
             index += 1
