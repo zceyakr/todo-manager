116e8b76cb7a085ee9fb1cb55398dfc9d233a5b4
Zackery Hitchcock <zackeryhitchcock@live.com>
2019-02-09 15:15:41 -0500

    It is now in a WHILE loop until the user quits

--- todos.py
+++ todos.py
@@ -3,31 +3,35 @@ import manager
 
 
 run = manager.Manager()
-action = input("""What do you want to do? Do you want to list, add, or complete?
-> """)
+while True:
+    action = input("""
+What do you want to do? Do you want to list, add, complete, or quit?
+    > """)
 
-if action == "list":
-    run.list()
+    if action == "list":
+        run.list()
 
-elif action == "add":
-    print("What task are you adding?")
-    new_task = input("> ")
-    add_task = item.Items(new_task)
-    run.add(add_task)
-    print(f"You added the task: {new_task}")
+    elif action == "add":
+        print("\nWhat task are you adding?")
+        new_task = input("> ")
+        add_task = item.Items(new_task)
+        run.add(add_task)
+        print(f"You added the task: {new_task}")
 
-elif action == "complete":
-    print("What is the name of the task you'd like to mark as completed?")
-    task_to_complete = input("> ")
-    run.complete(task_to_complete)
+    elif action == "complete":
+        print("\nWhat is the name of the task you'd like to mark as completed?")
+        task_to_complete = input("> ")
+        run.complete(task_to_complete)
 
-elif action == "quit":
-    print("Goodbye.")
-    quit()
+    elif action == "quit":
+        print("Goodbye.")
+        exit()
 
-else:
-    print("""Did you mean:
-    list - lists the current tasks in the file
-    add - let’s you add an additional task to your file
-    complete - Let’s you mark a task as complete if it is not completed
-    """)
+    else:
+        print("""
+Did you mean:
+        list - Lists the current tasks in the file.
+        add - Let’s you add an additional task to your file.
+        complete - Let’s you mark a task as complete if it is not completed.
+        quit - Quits out of the todo manager.
+        """)
