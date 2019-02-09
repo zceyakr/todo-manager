c8f0a2805b04d3d2f949076fec0eddd2daa6f436
Zackery Hitchcock <zackeryhitchcock@live.com>
2019-02-09 13:57:01 -0500

    Changed items class name to __init__ so the add feature works

--- item.py
+++ item.py
@@ -2,7 +2,7 @@ import datetime
 
 class Items(object):
 
-   def created_task(self, task):
+   def __init__(self, task):
         self.time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         self.is_task_complete = False
         self.task = task
